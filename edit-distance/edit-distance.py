fea445afc36985cc9a0b639e3c8b0c22f30fd3e8
Richard Nguyen <117381750+richardnguyen0715@users.noreply.github.com>
2026-04-09 21:26:34 +0700

    Add solution for edit-distance

---
+++ edit-distance/edit-distance.py
@@ -0,0 +1,26 @@
+def edit_distance(s1, s2):
+    m, n = len(s1), len(s2)
+    
+    # dp table
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    
+    # base cases
+    for i in range(m + 1):
+        dp[i][0] = i   # delete all characters
+    
+    for j in range(n + 1):
+        dp[0][j] = j   # insert all characters
+    
+    # fill table
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            else:
+                dp[i][j] = 1 + min(
+                    dp[i - 1][j],     # delete
+                    dp[i][j - 1],     # insert
+                    dp[i - 1][j - 1]  # replace
+                )
+    
+    return dp[m][n]
