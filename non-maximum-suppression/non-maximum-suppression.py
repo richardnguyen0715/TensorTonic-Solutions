7b50b3858039738d50a8b03e03ec84c34b82103b
Richard Nguyen <117381750+richardnguyen0715@users.noreply.github.com>
2026-09-13 15:10:53 +0700

    Solve non-maximum-suppression

--- non-maximum-suppression/non-maximum-suppression.py
+++ non-maximum-suppression/non-maximum-suppression.py
@@ -12,46 +12,35 @@ def nms(boxes: list, scores: list, iou_threshold: float) -> list:
 
     selected = []
 
-    # Những box hiện tại vẫn còn được xét
     remaining = set(range(len(boxes)))
 
     for score, idx in score_indexed:
 
-        # Box này đã bị box trước đó suppress
         if idx not in remaining:
             continue
-
-        boxA = boxes[idx]
-
-        # Chọn box A
+                    
         selected.append(idx)
         remaining.remove(idx)
 
-        # Kiểm tra các box còn lại
+        boxA = boxes[idx]
+
         for idx_2 in remaining.copy():
 
             boxB = boxes[idx_2]
 
-            # Intersection
             xL = max(boxA[0], boxB[0])
             yT = max(boxA[1], boxB[1])
             xR = min(boxA[2], boxB[2])
             yB = min(boxA[3], boxB[3])
 
-            width = max(0, xR - xL)
-            height = max(0, yB - yT)
-
-            intersection = width * height
-
-            # Area
+            A_intersection = max(0, xR - xL) * max(0, yB - yT)
             areaA = (boxA[2] - boxA[0]) * (boxA[3] - boxA[1])
             areaB = (boxB[2] - boxB[0]) * (boxB[3] - boxB[1])
 
-            # IoU
-            iou = intersection / (areaA + areaB - intersection)
+            IoU_AB = A_intersection / (areaA + areaB - A_intersection)
 
-            # Quá trùng → loại
-            if iou >= iou_threshold:
+            if IoU_AB >= iou_threshold:
                 remaining.remove(idx_2)
+
 
     return selected
